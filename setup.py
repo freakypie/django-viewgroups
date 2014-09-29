5c502c1777cba81701942dbbc96c67aac3b24636
John Leith <jleith@velocitywebworks.com>
2014-09-29 14:20:00 -0600

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-viewgroups',
-    version='0.1.0',
+    version='0.1.1',
     author='John Leith',
     author_email='leith.john@gmail.com',
     packages=find_packages(),
