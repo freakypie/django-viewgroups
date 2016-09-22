69a6f98f5d25fefd7539698430d0cd1466db43d2
Preston <pwag42@hotmail.com>
2016-09-22 14:08:41 -0600

    cleaned up staticfiles (#5)
    * moved inline duplication javascript to its own static file

---
+++ viewsets/static/scripts/add_more_inlines.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.form-inline > .add-more').click(function(event){
+        event.preventDefault();
+        var $inline_form = $(this).parent();
+        var $total_forms = $inline_form.find('[name*="TOTAL_FORMS"]');
+        var form_idx = $total_forms.val();
+        var $empty_form = $inline_form.find('.empty-form');
+        var $forms_list = $inline_form.find('.forms');
+        $forms_list.append(
+            $empty_form.clone().removeClass('empty-form').addClass('form').html(
+                $empty_form.html().replace(/__prefix__/g, form_idx)
+            )
+        );
+        $total_forms.val(parseInt(form_idx) + 1);
+    });
+});
